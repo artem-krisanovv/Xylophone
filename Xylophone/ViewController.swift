9a12d8316e032017c88b1080b1ba53191ebc027c
Artem Krisanov <a.krisanov1@mail.ru>
2025-04-08 21:11:39 +0300

    made my option before watching lesson

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,7 +18,41 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        if sender.backgroundColor == .systemRed {
+            playSound()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemOrange {
+            playSound1()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemYellow {
+            playSound2()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemGreen {
+            playSound3()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemBlue {
+            playSound4()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemIndigo {
+            playSound5()
+        } else {
+            print("Wrong Key")
+        }
+        if sender.backgroundColor == .systemPurple {
+            playSound6()
+        } else {
+            print("Wrong Key")
+        }
     }
     
     func playSound() {
@@ -26,5 +60,42 @@ class ViewController: UIViewController {
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
         
+    }
+    
+    func playSound1() {
+        let url = Bundle.main.url(forResource: "D", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
+    func playSound2() {
+        let url = Bundle.main.url(forResource: "E", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func playSound3() {
+        let url = Bundle.main.url(forResource: "F", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func playSound4() {
+        let url = Bundle.main.url(forResource: "G", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func playSound5() {
+        let url = Bundle.main.url(forResource: "A", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    func playSound6() {
+        let url = Bundle.main.url(forResource: "B", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
