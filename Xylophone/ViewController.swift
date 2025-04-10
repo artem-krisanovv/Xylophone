7f9667a33295b3075a35251df3ca095d8717a516
Artem Krisanov <a.krisanov1@mail.ru>
2025-04-10 20:58:16 +0300

    Added array option

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,23 +12,18 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var player: AVAudioPlayer!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+
+    let soundFiles = ["C", "D", "E", "F", "G", "A", "B"]
     
     @IBAction func keyPressed(_ sender: UIButton) {
         
-        playSound(buttonName: sender.titleLabel!.text!)
-        
-    }
-    
-    func playSound(buttonName: String) {
-        let url = Bundle.main.url(forResource: buttonName, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
-        
-    }
-}
+        let index = sender.tag
+                let fileName = soundFiles[index]
+                
+                let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+                player = try? AVAudioPlayer(contentsOf: url!)
+                player?.play()
+            }
+        }
     
     
